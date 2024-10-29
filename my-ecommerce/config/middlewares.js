873221acff7e852e97980cfa1dd8e0443900236d
Harito_280 <ezequiel.hao@gmail.com>
2024-10-29 10:43:55 -0300

    first commit

---
+++ my-ecommerce/config/middlewares.js
@@ -0,0 +1,37 @@
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "yourBucketName.s3.yourRegion.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "yourBucketName.s3.yourRegion.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
